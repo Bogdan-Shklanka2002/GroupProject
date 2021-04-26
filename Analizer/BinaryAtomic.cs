8aba55f86d17497ea50aebd9bb17558f4160f636
veretilnuktar <veretilnuktar@gmail.com>
2021-04-26 18:23:26 +0300

    ADD_FILES

---
+++ Analizer/BinaryAtomic.cs
@@ -0,0 +1,29 @@
+﻿namespace Analizer
+{
+    internal class BinaryAtomic : BinaryExpression
+    {
+        protected BinaryAtomic()
+        {
+        }
+
+
+        public BinaryAtomic(double value)
+        {
+            Value = value;
+        }
+
+
+        public override double Value
+        {
+            get;
+            protected set;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+
+    }
+}
---
+++ Analizer/BinaryExpression.cs
@@ -0,0 +1,30 @@
+﻿namespace Analizer
+{
+    internal class BinaryExpression
+    {
+        protected BinaryExpression()
+        {
+        }
+
+
+        public BinaryExpression(BinaryExpression left, BinaryExpression right,
+            Operator op)
+        {
+            Left = left; Right = right; Operator = op;
+        }
+
+
+        public virtual double Value
+        {
+            get
+            {
+                return Operator(Left.Value, Right.Value);
+            }
+            protected set { }
+        }
+
+        public BinaryExpression Left;
+        public BinaryExpression Right;
+        public Operator Operator;
+    }
+}
