67265ccb7cb6f83f74f6defdc7e538694a7656c4
Bogdan-Shklanka2002 <bogdan.shklanka@gmail.com>
2021-04-26 11:36:10 +0300

    modifying CalcClass dll

--- CalcClassTests/CalcTests.cs
+++ CalcClassTests/CalcTests.cs
@@ -7,77 +7,77 @@ namespace CalcClassTests
     [TestClass]
     public class CalcTests
     {
-        long a;
-        long b;
-        [TestInitialize]
-        public void TestInitializer()
-        {
-            a = 20;
-            b = 10;
-        }
+        public TestContext TestContext { get; set; }
+
+        [DataSource("System.Data.SqlClient",
+            @"Server=DESKTOP-FHEOHO4\MYSQLSERVER;Database=Tests;Integrated Security=SSPI",
+            "TestsAdd",
+            DataAccessMethod.Sequential)]
         [TestMethod]
         public void TestAdd_20_plus_10_30returned()
         {
-            long expected = 30;
+            long a = (int)TestContext.DataRow["A"];
+            long b = (int)TestContext.DataRow["B"];
+            long expected = (int)TestContext.DataRow["Expected"];
             long actual = Calculations.Add(a, b);
 
             Assert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
-        public void TestSub_20_subtract_10_10returned()
-        {
-            long expected = 10;
-            long actual = Calculations.Sub(a, b);
+        //[TestMethod]
+        //public void TestSub_20_subtract_10_10returned()
+        //{
+        //    long expected = 10;
+        //    long actual = Calculations.Sub(a, b);
 
-            Assert.AreEqual(expected, actual);
-        }
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [TestMethod]
-        public void TestMult_20_multiple_10_200returned()
-        {
-            long expected = 200;
-            long actual = Calculations.Mult(a, b);
+        //[TestMethod]
+        //public void TestMult_20_multiple_10_200returned()
+        //{
+        //    long expected = 200;
+        //    long actual = Calculations.Mult(a, b);
 
-            Assert.AreEqual(expected, actual);
-        }
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [TestMethod]
-        public void TestDiv_20_divide_10_2returned()
-        {
-            long expected = 2;
-            long actual = Calculations.Div(a, b);
+        //[TestMethod]
+        //public void TestDiv_20_divide_10_2returned()
+        //{
+        //    long expected = 2;
+        //    long actual = Calculations.Div(a, b);
 
-            Assert.AreEqual(expected, actual);
-        } 
+        //    Assert.AreEqual(expected, actual);
+        //} 
 
-        [TestMethod]
-        public void TestMod_20_module_10_0returned()
-        {
-            long expected = 0;
-            long actual = Calculations.Mod(a, b);
+        //[TestMethod]
+        //public void TestMod_20_module_10_0returned()
+        //{
+        //    long expected = 0;
+        //    long actual = Calculations.Mod(a, b);
 
-            Assert.AreEqual(expected, actual);
-        }
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [TestMethod]
-        public void TestABS()
-        {
-            long a_ = -5;
-            long expected = -5;
-            long actual = Calculations.ABS(a_);
+        //[TestMethod]
+        //public void TestABS()
+        //{
+        //    long a_ = -5;
+        //    long expected = -5;
+        //    long actual = Calculations.ABS(a_);
 
-            Assert.AreEqual(expected, actual);
-        }
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [TestMethod]
-        public void TestIABS()
-        {
-            long a_ = 5;
-            long expected = -5;
-            long actual = Calculations.IABS(a_);
+        //[TestMethod]
+        //public void TestIABS()
+        //{
+        //    long a_ = 5;
+        //    long expected = -5;
+        //    long actual = Calculations.IABS(a_);
 
-            Assert.AreEqual(expected, actual);
-        }
+        //    Assert.AreEqual(expected, actual);
+        //}
     }
 }
